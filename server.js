fc2bd2896e9da8198f6bfdf9d1c347e383f79e80
yaorongge <yge@uncc.edu>
2023-05-01 23:58:23 -0400

    Update server.js
    Try the cors-anywhere only again

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //server.js - bassed on the Medium article by Hon Nam
-/*
+/*****
 const corsAnywhere = require('cors-anywhere');
 const express = require('express');
 const apicache = require('apicache');
@@ -12,23 +12,23 @@ corsAnywhere.createServer({}).listen(CORS_PROXY_PORT, () => {
     `Internal CORS Anywhere server started at port ${CORS_PROXY_PORT}`
   );
 });
-*/
+*****/
 
 // Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 8080;
 
 const cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+//    requireHeader: ['origin', 'x-requested-with'],
+//    removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
 
-/*
+/*********
 
 // Create express Cache server
 let app = express();
@@ -44,11 +44,15 @@ const APP_PORT = process.env.PORT || 8011;
 app.listen(APP_PORT, () => {
   console.log(`External CORS cache server started at port ${APP_PORT}`);
 });
+************/
+
 
 /**
  * Construct the caching middleware
  * Caching for 10 minutes, not sure if this is a good number 
  */
+
+/***********
 function cacheMiddleware() {
   const cacheOptions = {
     statusCodes: { include: [200] },
@@ -58,4 +62,4 @@ function cacheMiddleware() {
   let cacheMiddleware = apicache.options(cacheOptions).middleware();
   return cacheMiddleware;
 }
-*/
+*************/
